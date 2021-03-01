a11f8434692d29893de9f1a0ecf1b2591ca6f9a7
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2021-03-02 02:28:51 +0300

    donors breakpoints collected

--- DonorAnalyzer.py
+++ DonorAnalyzer.py
@@ -8,7 +8,18 @@ def analyze_donor(donor, cur):
         f'SELECT * FROM sv_intra \
         WHERE donor_id = \'{donor}\' \
         AND chr SIMILAR TO \'{regex_up_to_21}\'')
-
     rows = cur.fetchall()
+
+    chr_bins_map = {}
+
     for row in rows:
-        print(row)
+        chr, bp1, bp2, _ = row
+        chr_breakpoints = chr_bins_map.get(chr, [])
+        if not chr_breakpoints:
+            chr_bins_map[chr] = [(bp1, bp2)]
+        else:
+            chr_bins_map[chr].append((bp1, bp2))
+
+    print(donor)
+    print(chr_bins_map)
+    print()
